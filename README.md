88552dc12176a0ceb1eb300e653e4a877bc2aa13
MrBridgesStem <92792644+MrBridgesStem@users.noreply.github.com>
2021-11-05 11:00:52 -0400

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://mrbridgesstem.github.io/yahtzeefinalcomplete/](https://mrbridgesstem.github.io/yahtzeefinalcomplete/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/mrbridgesstem/yahtzeefinalcomplete** and import
+
+## Edit this project ![Build status badge](https://github.com/mrbridgesstem/yahtzeefinalcomplete/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/mrbridgesstem/yahtzeefinalcomplete** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/mrbridgesstem/yahtzeefinalcomplete/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,143 @@
+let StoredDice3 = 0
+let Dice3 = 0
+let StoredDice1 = 0
+let Dice1 = 0
+let StoredDice5 = 0
+let Dice5 = 0
+let StoredDice2 = 0
+let Dice2 = 0
+let StoredDice4 = 0
+let Dice4 = 0
+let ShakeCount = 0
+input.onPinPressed(TouchPin.P0, function () {
+    if (StoredDice3 > 0) {
+        StoredDice3 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        StoredDice3 = Dice3
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (StoredDice1 > 0) {
+        StoredDice1 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        StoredDice1 = Dice1
+    }
+})
+input.onPinPressed(TouchPin.P2, function () {
+    if (StoredDice5 > 0) {
+        StoredDice5 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        StoredDice5 = Dice5
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (StoredDice2 > 0) {
+        StoredDice2 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        StoredDice2 = Dice2
+    }
+})
+input.onPinPressed(TouchPin.P1, function () {
+    if (StoredDice4 > 0) {
+        StoredDice4 = 0
+    } else {
+        basic.showIcon(IconNames.Yes)
+        StoredDice4 = Dice4
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    ShakeCount = ShakeCount + 1
+    Dice1 = randint(1, 6)
+    Dice2 = randint(1, 6)
+    Dice3 = randint(1, 6)
+    Dice4 = randint(1, 6)
+    Dice5 = randint(1, 6)
+})
+basic.forever(function () {
+    if (ShakeCount > 3 || StoredDice1 != 0 && StoredDice2 != 0 && (StoredDice3 != 0 && (StoredDice4 != 0 && StoredDice5 != 0))) {
+        ShakeCount = 0
+        basic.showString("Shake")
+    } else {
+        if (input.buttonIsPressed(Button.AB)) {
+            if (StoredDice1 > 0) {
+                basic.showString("S#1:")
+                basic.showNumber(StoredDice1)
+            } else {
+                basic.showString("S#1:")
+                basic.showIcon(IconNames.No)
+            }
+            if (StoredDice2 > 0) {
+                basic.showString("S#2:")
+                basic.showNumber(StoredDice2)
+            } else {
+                basic.showString("S#2:")
+                basic.showIcon(IconNames.No)
+            }
+            if (StoredDice3 > 0) {
+                basic.showString("S#3:")
+                basic.showNumber(StoredDice3)
+            } else {
+                basic.showString("S#3:")
+                basic.showIcon(IconNames.No)
+            }
+            if (StoredDice4 > 0) {
+                basic.showString("S#4:")
+                basic.showNumber(StoredDice4)
+            } else {
+                basic.showString("S#4:")
+                basic.showIcon(IconNames.No)
+            }
+            if (StoredDice5 > 0) {
+                basic.showString("S#5:")
+                basic.showNumber(StoredDice5)
+            } else {
+                basic.showString("S#5:")
+                basic.showIcon(IconNames.No)
+            }
+        } else {
+            if (Dice1 == 0) {
+                basic.showString("Shake")
+            } else {
+                if (StoredDice1 > 0) {
+                    basic.showString("D#1:")
+                    basic.showNumber(StoredDice1)
+                } else {
+                    basic.showString("D#1:")
+                    basic.showNumber(Dice1)
+                }
+                if (StoredDice2 > 0) {
+                    basic.showString("D#2:")
+                    basic.showNumber(StoredDice2)
+                } else {
+                    basic.showString("D#2:")
+                    basic.showNumber(Dice2)
+                }
+                if (StoredDice3 > 0) {
+                    basic.showString("D#3:")
+                    basic.showNumber(StoredDice3)
+                } else {
+                    basic.showString("D#3:")
+                    basic.showNumber(Dice3)
+                }
+                if (StoredDice4 > 0) {
+                    basic.showString("D#4:")
+                    basic.showNumber(StoredDice4)
+                } else {
+                    basic.showString("D#4:")
+                    basic.showNumber(Dice4)
+                }
+                if (StoredDice5 > 0) {
+                    basic.showString("D#5:")
+                    basic.showNumber(StoredDice5)
+                } else {
+                    basic.showString("D#5:")
+                    basic.showNumber(Dice5)
+                }
+            }
+        }
+    }
+})
